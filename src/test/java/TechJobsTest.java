39087edb9ab83cf9ee29f8359a4303d362800467
Lane Flint <110209944+golanego505@users.noreply.github.com>
2022-09-26 19:48:01 -0500

    Fixaroo

--- src/test/java/TechJobsTest.java
+++ src/test/java/TechJobsTest.java
@@ -56,7 +56,8 @@ public class TechJobsTest {
         String input = "0\n2\nChicago\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testPrintJobsNoResults.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
+
     }
 
     @Test
@@ -64,7 +65,7 @@ public class TechJobsTest {
         String input = "0\n0\nRuby\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testFindByValue.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
@@ -72,7 +73,7 @@ public class TechJobsTest {
         String input = "0\n3\nnew YORk\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testCaseInsensitiveSearch.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
 
